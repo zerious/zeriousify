8610180241ce8618119085eb74aa74fe78460e2a
Sam Eubank <sameubank+zerious@gmail.com>
2014-05-28 16:12:57 -0700

    Removed string type from API comment

--- zeriousify.js
+++ zeriousify.js
@@ -2,7 +2,6 @@ var api = module.exports;
 
 /**
  * Expose the version to module users.
- * @type {string}
  */
 api.version = require('./package.json').version;
 
@@ -10,5 +9,5 @@ api.version = require('./package.json').version;
  * Test a module that requires zeriousify.
  */
 api.test = function () {
-	require('./test/zeriousifyTest');
+  require('./test/zeriousifyTest');
 }
